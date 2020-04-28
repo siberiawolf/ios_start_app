b9b690669a411dc5805509fc7f32bf50a1cc93c5
siberiawolf <wolf199103@vip.qq.com>
2020-04-28 11:51:28 +0800

    将ios代码恢复到IOS12，参考帖子https://segmentfault.com/n/1330000022494782

--- SampleApp/AppDelegate.m
+++ SampleApp/AppDelegate.m
@@ -45,25 +45,16 @@
     
     self.window.rootViewController = tabbarController;  // 显示最基本的tabbarController
     [self.window makeKeyAndVisible];*/
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    self.window.rootViewController = [[ViewController alloc] init];;
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
 
 
 @end
