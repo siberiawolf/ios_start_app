a6e65c7bcf5639138c6af9a410e1dd409c74fc1c
siberiawolf <wolf199103@vip.qq.com>
2020-04-28 22:04:43 +0800

    完成《结合视图和控制器构建Tabbar样式页面》 这一小节中，老师的答复帮我解决了不少困惑和迷茫，继续学习🆙 同时这一节中学会了引用icon.bundle。

--- SampleApp/AppDelegate.m
+++ SampleApp/AppDelegate.m
@@ -29,21 +29,35 @@
     // 创建4个UIViewController
     UIViewController *controller1 = [[UIViewController alloc] init];
     controller1.view.backgroundColor = [UIColor redColor];
+    controller1.tabBarItem.title = @"新闻";
+    controller1.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/page@2x.png"];
+    controller1.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/page_selected@2x.png"];
     
     UIViewController *controller2 = [[UIViewController alloc] init];
     controller2.view.backgroundColor = [UIColor yellowColor];
+    controller2.tabBarItem.title = @"视频";
+    controller2.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/video@2x.png"];
+    controller2.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/video_selected@2x.png"];
+
 
     UIViewController *controller3 = [[UIViewController alloc] init];
     controller3.view.backgroundColor = [UIColor greenColor];
+    controller3.tabBarItem.title = @"推荐";
+    controller3.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/like@2x.png"];
+    controller3.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/like_selected@2x.png"];
 
     
     UIViewController *controller4 = [[UIViewController alloc] init];
     controller4.view.backgroundColor = [UIColor lightGrayColor];
+    controller4.tabBarItem.title = @"我的";
+    controller4.tabBarItem.image = [UIImage imageNamed:@"icon.bundle/home@2x.png"];
+    controller4.tabBarItem.selectedImage = [UIImage imageNamed:@"icon.bundle/home_selected@2x.png"];
 
     
        
     // 将4个UIViewController添加到UITabBarController中
-    [tabbarController setViewControllers:@[controller1, controller2, controller3, controller4]];
+    [tabbarController setViewControllers:@[controller2, controller1, controller3, controller4]];
+    // 底部导航的顺序完全是由堆栈的顺序决定
     
     self.window.rootViewController = tabbarController;  // 显示最基本的tabbarController
     
