5dbe9099af75dfe919c4bb72af843bd89728f16c
siberiawolf <wolf199103@vip.qq.com>
2020-05-10 09:30:08 +0800

    集成和使用开源网络框架 AFNetworking

--- SampleApp/loader/GTListLoader.m
+++ SampleApp/loader/GTListLoader.m
@@ -7,32 +7,42 @@
 //
 
 #import "GTListLoader.h"
+#import <AFNetworking.h>
 
 @implementation GTListLoader
 
-- (void) loadListData{
+- (void)loadListData {
+    
+//    使用AFNetworking 处理网络请求
+    [[AFHTTPSessionManager manager] GET:@"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json" parameters:nil headers:nil progress:^(NSProgress *_Nonnull downloadProgress) {
+        NSLog(@"AFNetworking  进度条");
+    } success:^(NSURLSessionDataTask *_Nonnull task, id _Nullable responseObject) {
+        NSLog(@"AFNetworking 请求成功");
+    } failure:^(NSURLSessionDataTask *_Nullable task, NSError *_Nonnull error) {
+        NSLog(@"AFNetworking 请求失败");
+    }];
+    
+//    使用系统自带的方式处理网络请求
+
 //    NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
-    NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
-    NSURL *listURL = [NSURL URLWithString:urlString];
-    
-    
-    
-    // 使用系统自带的sharedSession创建一个session
-    NSURLSession *session = [NSURLSession sharedSession];
-    
+//    NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
+//    NSURL *listURL = [NSURL URLWithString:urlString];
+
+//     使用系统自带的sharedSession创建一个session
+//    NSURLSession *session = [NSURLSession sharedSession];
+
 //     dataTask属于session；同时封装了listRequest
 //    __unused NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
 //    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:listRequest];
-    
+
 //    通过Handler block处理response
-    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-        NSLog(@"请求成功"); 
-    }];
-    
-    [dataTask resume]; // 恢复task(执行task)
-    
-    NSLog(@"");
-    
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData *_Nullable data, NSURLResponse *_Nullable response, NSError *_Nullable error) {
+//        NSLog(@"请求成功");
+//    }];
+//
+//    [dataTask resume]; // 恢复task(执行task)
+//
+//    NSLog(@"");
 }
 
 @end
