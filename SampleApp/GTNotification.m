0ba0bad4cab20a5cf089fc89d851aea4d21406a7
siberiawolf <wolf199103@vip.qq.com>
2020-05-24 17:43:54 +0800

    iOS图标管理和动态更新

--- SampleApp/GTNotification.m
+++ SampleApp/GTNotification.m
@@ -32,10 +32,10 @@
     center.delegate = self;
     [center requestAuthorizationWithOptions:UNAuthorizationOptionBadge|UNAuthorizationOptionSound completionHandler:^(BOOL granted, NSError * _Nullable error) {
         if (granted) {
-//            [self _pushLocalNotification];
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [[UIApplication sharedApplication] registerForRemoteNotifications];
-            });
+            [self _pushLocalNotification];
+//            dispatch_async(dispatch_get_main_queue(), ^{
+//                [[UIApplication sharedApplication] registerForRemoteNotifications];
+//            });
         }
     }];
 
